d586dcbd315a4fb17fc1d40b58a37708aef97898
MuhammadSaudFarooq <muhammadsaudfarooque@gmail.com>
2024-07-30 22:44:05 +0500

    Result has been updated

--- renderer.js
+++ renderer.js
@@ -11,34 +11,133 @@ $(document).ready(function () {
         e.preventDefault();
         let _this = $(this);
         let form_input = _this.find('input');
-        let url = form_input.val();
+        let website = form_input.val();
         let pattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,6})(\/[a-zA-Z0-9#-\/]*)?$/;
         let preloader = `<div class="preloader"><div><div class="preloader-wrap"><div></div><div></div><div></div><div></div></div></div></div>`;
 
 
-        if (pattern.test(url)) {
+        if (pattern.test(website)) {
+            let strategy = 'desktop';
+
             form_input.removeClass('invalid');
             $('.error-msg').remove();
 
             // For Desktop
             $.ajax({
-                url: `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&strategy=desktop&category=performance&category=accessibility`,
+                url: pageInsightUrl(website, strategy),
                 method: 'GET',
                 beforeSend: function () {
                     $(preloader).insertAfter(form_input);
                     $(form_input).attr('disabled', 'disabled');
                 },
                 success: function (res) {
+                    let strategy = 'mobile';
+                    let desktop_result = res.lighthouseResult.categories.performance.score;
+                    desktop_result = Math.trunc(desktop_result * 100);
+
                     $(form_input).removeAttr('disabled');
                     $('.preloader').remove();
 
-                    console.log(res.lighthouseResult.categories.performance.score);
+                    // For Mobile
+                    $.ajax({
+                        url: pageInsightUrl(website, strategy),
+                        method: 'GET',
+                        beforeSend: function () {
+                            $(preloader).insertAfter(form_input);
+                            $(form_input).attr('disabled', 'disabled');
+                        },
+                        success: function (res) {
+                            let mobile_result = res.lighthouseResult.categories.performance.score;
+                            mobile_result = Math.trunc(mobile_result * 100);
+
+                            $(form_input).removeAttr('disabled');
+                            $('.preloader').remove();
+
+                            // Colors for desktop
+                            let color_desktop = '';
+                            let border_desktop = '';
+                            let bg_desktop = '';
+
+                            if (desktop_result >= 90 && desktop_result <= 100) {
+                                color_desktop = 'var(--good)';
+                                border_desktop = 'var(--good)';
+                                bg_desktop = 'var(--good-bg)';
+                            }
+                            else if (desktop_result >= 50 && desktop_result <= 89) {
+                                color_desktop = 'var(--average)';
+                                border_desktop = 'var(--average)';
+                                bg_desktop = 'var(--average-bg)';
+                            }
+                            else {
+                                color_desktop = 'var(--poor)';
+                                border_desktop = 'var(--poor)';
+                                bg_desktop = 'var(--poor-bg)';
+                            }
+
+                            // Colors for mobile
+                            let color_mobile = '';
+                            let border_mobile = '';
+                            let bg_mobile = '';
+
+                            if (mobile_result >= 90 && mobile_result <= 100) {
+                                color_mobile = 'var(--good)';
+                                border_mobile = 'var(--good)';
+                                bg_mobile = 'var(--good-bg)';
+                            }
+                            else if (mobile_result >= 50 && mobile_result <= 89) {
+                                color_mobile = 'var(--average)';
+                                border_mobile = 'var(--average)';
+                                bg_mobile = 'var(--average-bg)';
+                            }
+                            else {
+                                color_mobile = 'var(--poor)';
+                                border_mobile = 'var(--poor)';
+                                bg_mobile = 'var(--poor-bg)';
+                            }
+
+                            let desktop_template = `<div class="pro_bar">
+                                                        <div class="prog-1">  
+                                                            <div class="c100 p${desktop_result} big">
+                                                                <span>${desktop_result}</span>
+                                                                <div class="sl">
+                                                                    <div class="slice">
+                                                                    <div class="bar"></div>
+                                                                    <div class="fill"></div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <h3 class="score_text">Dektop</h3>
+                                                    </div>`;
+                            let mobile_template = `<div class="pro_bar">
+                                                        <div class="prog-1">  
+                                                            <div class="c100 p${mobile_result} big">
+                                                                <span>${mobile_result}</span>
+                                                                <div class="sl">
+                                                                    <div class="slice">
+                                                                    <div class="bar"></div>
+                                                                    <div class="fill"></div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <h3 class="score_text">Mobile</h3>
+                                                    </div>`;
+
+                            $('.main_center_box').append(mobile_template);
+                            $('.main_center_box').append(desktop_template);
+                        },
+                        error: function (error) {
+                            $(form_input).removeAttr('disabled');
+                            $('.preloader').remove();
+                            console.error('Mobile Error:', error);
+                        }
+                    });
                 },
                 error: function (error) {
                     $(form_input).removeAttr('disabled');
                     $('.preloader').remove();
-
-                    console.error('Error:', error);
+                    console.error('Desktop Error:', error);
                 }
             });
         } else {
@@ -46,9 +145,10 @@ $(document).ready(function () {
             $(`<span class="error-msg">Invalid URL</span>`).insertAfter(form_input);
         }
     });
-
-
-    // setInterval(() => {
-    //     console.log('Connection Status', navigator.onLine);
-    // }, 1000);
 });
+
+// Functions
+let pageInsightUrl = (website, strategy) => {
+    let url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${website}&strategy=${strategy}&category=performance&category=accessibility`;
+    return url;
+}
